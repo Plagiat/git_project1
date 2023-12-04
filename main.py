6a7c1808eb23932cdd0cbac2e6ef2d249817ee54
Антон <sliii@mail.ru>
2023-12-04 17:04:41 +0700

    Add author

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+# Author
 def new_function():
     print("Some realisation")
 
